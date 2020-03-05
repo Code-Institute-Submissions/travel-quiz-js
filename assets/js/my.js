320ec029deee85f69a27ba4b87c1466d80ff1b17
sw1ckham <sphwick@gmail.com>
2020-03-05 15:52:05 +0000

    Added speech marks to questions and answers

--- assets/js/my.js
+++ assets/js/my.js
@@ -68,69 +68,99 @@
         switch (answerArray) {
             case 'TTTTT':
                 countryToGoTo = 'HAWAII!';
+                break;
             case 'TTTTF':
                 countryToGoTo = 'LAOS!';
+                break;
             case 'TTTFT':
                 countryToGoTo = 'SANTIAGO, U.S.A!';
+                break;
             case 'TTTFF':
                 countryToGoTo = 'VALENCIA, SPAIN!';
+                break;
             case 'TTFTT':
                 countryToGoTo = '5';
+                break;
             case 'TTFTF':
                 countryToGoTo = '6';
+                break;
             case 'TTFFT':
                 countryToGoTo = '7';
+                break;
             case 'TTFFF':
                 countryToGoTo = '8';
+                break;
             case 'TFTTT':
                 countryToGoTo = '9';
+                break;
             case 'TFTTF':
                 countryToGoTo = '10';
+                break;
             case 'TFTFT':
                 countryToGoTo = '11';
+                break;
             case 'TFTFF':
                 countryToGoTo = '12';
+                break;
             case 'TFFTT':
                 countryToGoTo = '13';
+                break;
             case 'TFFTF':
                 countryToGoTo = '14';
+                break;
             case 'TFFFT':
                 countryToGoTo = '15';
+                break;
             case 'TFFFF':
                 countryToGoTo = '16';
+                break;
             case 'FTTTT':
                 countryToGoTo = '17';
+                break;
             case 'FTTTF':
                 countryToGoTo = '18';
+                break;
             case 'FTTFT':
                 countryToGoTo = '19';
+                break;
             case 'FTTFF':
                 countryToGoTo = '20';
+                break;
             case 'FTFTT':
                 countryToGoTo = '21';
+                break;
             case 'FTFTF':
                 countryToGoTo = '22';
+                break;
             case 'FTFFT':
                 countryToGoTo = '23';
+                break;
             case 'FTFFF':
                 countryToGoTo = '24';
+                break;
             case 'FFTTT':
                 countryToGoTo = '25';
+                break;
             case 'FFTTF':
                 countryToGoTo = '26';
+                break;
             case 'FFTFT':
                 countryToGoTo = '27';
+                break;
             case 'FFTFF':
                 countryToGoTo = '28';
+                break;
             case 'FFFTT':
                 countryToGoTo = '29';
+                break;
             case 'FFFTF':
                 countryToGoTo = '30';
+                break;
             case 'FFFFT':
                 countryToGoTo = '31';
+                break;
             case 'FFFFF':
                 countryToGoTo = '32';
-
         }
 
         resultsContainer.innerHTML = `${countryToGoTo}!`;
@@ -169,7 +199,15 @@
     })
 
     $("#submit").click(function(){
-        $("button").hide();
+        $("#submit").hide();
+    })
+
+    $("#submit").click(function(){
+        $("#previous").hide();
+    })
+
+    $("#submit").click(function(){
+        $("#next").hide();
     })
 
     $("#results").hide();
@@ -192,43 +230,43 @@
     const previousButton = document.getElementById("previous");
     const myQuestions = [
         {
-            question: "I want to be in a hot and sunny country.",
+            question: '"I want to be in a hot and sunny country."',
             answers: {
-                A: "Yes! I love the heat.",
-                B: "No, I prefer the cooler side of life."
+                A: '"Yes! I love the heat."',
+                B: '"No, I prefer the cooler side of life."'
             },
             correctAnswer: "A" // we need to refer to the correct answer so we can use this to make sure T(rue) or F(alse) is added to the answer string.
         },
 
         {
-            question: "I want to be in a mountain range landscape.",
+            question: '"I want to be in a mountain range landscape."',
             answers: {
-                A: "Sounds great!",
-                B: "No thanks, mountains aren't for me."
+                A: '"Sounds great!"',
+                B: '"No thanks, mountains are not for me."'
             },
             correctAnswer: "A"
         },
         {
-            question: "I want to be able to speak English.",
+            question: '"I want to be able to speak English."',
             answers: {
-                A: "Yes, that is a definite must!",
-                B: "No I am happy to learn the lingo."
+                A: '"Yes, that is a definite must!"',
+                B: '"No I am happy to learn the lingo."'
             },
             correctAnswer: "A"
         },
         {
-            question: "I want to be surrounded with wildlife!",
+            question: '"I want to be surrounded with wildlife!"',
             answers: {
-                A: "Absolutely!",
-                B: "No I prefer the a city environment."
+                A: '"Absolutely!"',
+                B: '"No I prefer the a city environment."'
             },
             correctAnswer: "A"
         },
         {
-            question: "I want to participate in extreme sports whilst I am away",
+            question: '"I want to participate in extreme sports whilst I am away"',
             answers: {
-                A: "Sounds right up my street.",
-                B: "Absolutely not!"
+                A: '"Sounds right up my street."',
+                B: '"Absolutely not!"'
             },
             correctAnswer: "A"
         }
@@ -251,15 +289,15 @@
 
 /*-- JavaScript for Google Maps ---*/
 
-//     function initMap() {
-//     var map = new google.maps.Map(document.getElementById("map"), {
-//         zoom: 3,
-//         center: {
-//             lat: 46.619261,     // add a case statement with all lng and lat, depends on the tf string.. 
-//             lng: -33.1344766
-//         }
-//     });
-// }
+    function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,     // add a case statement with all lng and lat, depends on the tf string.. 
+            lng: -33.1344766
+        }
+    });
+}
 
 
 // 
